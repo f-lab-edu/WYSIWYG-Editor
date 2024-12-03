622ce24a868df1c449929956c19a15ba4fadce39
PKyes44 <rugate23@gmail.com>
2024-12-03 15:24:31 +0900

    feat: create frame component function

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
-function component() {
+const editorFrame = document.getElementById("editor");
+editorFrame.style.display = "grid";
+
+function getEditorArea() {
   const element = document.createElement("div");
 
-  element.innerHTML = "Hello Typescript";
+  element.innerHTML = "EDITOR";
+  element.style.border = "1px solid black";
 
   return element;
 }
 
-document.body.appendChild(component());
+function getTextarea() {
+  const element = document.createElement("textarea");
+
+  element.style.border = "1px solid black";
+  element.style.borderTop = "none";
+  element.style.outline = "none";
+  element.style.padding = "5px 5px";
+
+  return element;
+}
+
+editorFrame.appendChild(getEditorArea());
+editorFrame.appendChild(getTextarea());
